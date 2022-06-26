0b3c5dc1513389e55d3c46b95105483f28a380d6
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-06-26 01:49:25 -0400

    Set up Android FHIR Questionnaires (#2)
    * Launches FHIR Questionnaire from Task view
    
    * Task button sends FHIR survey filename to SurveyActivity
    
    * Extract FHIR response

--- app/src/main/java/edu/stanford/cardinalkit/presentation/MainActivity.kt
+++ app/src/main/java/edu/stanford/cardinalkit/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +13,7 @@ import edu.stanford.cardinalkit.presentation.navigation.CKNavHost
 import edu.stanford.cardinalkit.presentation.navigation.Screens
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/edu/stanford/cardinalkit/presentation/tasks/TasksScreen.kt
+++ app/src/main/java/edu/stanford/cardinalkit/presentation/tasks/TasksScreen.kt
@@ -1,5 +1,7 @@
 package edu.stanford.cardinalkit.presentation.tasks
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import edu.stanford.cardinalkit.R
-import edu.stanford.cardinalkit.presentation.tasks.components.TaskItem
+import edu.stanford.cardinalkit.presentation.surveys.SurveyActivity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,16 +34,30 @@ fun TasksScreen() {
             )
         },
         content = { contentPadding ->
+            val context = LocalContext.current
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(contentPadding),
-                verticalArrangement = Arrangement.SpaceAround,
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
-                TaskItem()
+                Button(
+                    modifier = Modifier.padding(20.dp),
+                    onClick = {
+                        openSurvey("single_choice_questionnaire.json", context)
+                }){
+                    Text("Take A Survey")
+                }
             }
         }
     )
+}
 
+fun openSurvey(name: String, context: Context) {
+    val intent = Intent(context, SurveyActivity::class.java).apply {
+        putExtra("edu.stanford.cardinalkit.SURVEY_NAME", name)
+    }
+    context.startActivity(intent)
 }
