1d4e514cecd70445bc2a7cee5b3e2449d818aaa9
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-07-10 15:58:19 -0400

    Add date fields to CKTask

--- app/src/main/java/edu/stanford/cardinalkit/domain/models/tasks/CKTask.kt
+++ app/src/main/java/edu/stanford/cardinalkit/domain/models/tasks/CKTask.kt
@@ -7,5 +7,7 @@ data class CKTask(
     val title: String = "",
     val description: String = "",
     val context: String = "",
+    var createdDate: Date? = null,
+    var updatedDate: Date? = null,
     val isActive: Boolean = true
 )
