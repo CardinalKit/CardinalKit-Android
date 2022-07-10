d828660fbbc91c5a7cedad7a105923659cd971ab
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-07-10 12:16:57 -0400

    Set up TasksViewModel

--- app/src/main/java/edu/stanford/cardinalkit/presentation/tasks/TasksViewModel.kt
+++ app/src/main/java/edu/stanford/cardinalkit/presentation/tasks/TasksViewModel.kt
@@ -1,13 +1,26 @@
 package edu.stanford.cardinalkit.presentation.tasks
 
-import android.content.Context
-import android.content.Intent
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.stanford.cardinalkit.common.Constants
-import edu.stanford.cardinalkit.presentation.surveys.SurveyActivity
+import edu.stanford.cardinalkit.domain.models.Response
+import edu.stanford.cardinalkit.domain.models.tasks.CKTask
+import edu.stanford.cardinalkit.domain.use_cases.tasks.TasksUseCases
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TasksViewModel @Inject constructor(): ViewModel() {
+class TasksViewModel @Inject constructor(
+    private var useCases: TasksUseCases
+): ViewModel() {
+    private val _tasksState = mutableStateOf<Response<List<CKTask>>>(Response.Loading)
+    val tasksState: State<Response<List<CKTask>>> = _tasksState
+
+    private fun getTasks() = viewModelScope.launch {
+        useCases.getTasks().collect { response ->
+            _tasksState.value = response
+        }
+    }
 }
