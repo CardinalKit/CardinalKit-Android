0723eab5648cb7106cb0f318b5e145794a47303f
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-07-11 01:38:59 -0400

    Add mock task repository for unit tests

---
+++ app/src/test/java/edu/stanford/cardinalkit/domain/use_cases/tasks/data/repository/FakeTasksRepository.kt
@@ -0,0 +1,22 @@
+package edu.stanford.cardinalkit.domain.use_cases.tasks.data.repository
+
+import edu.stanford.cardinalkit.domain.models.Response
+import edu.stanford.cardinalkit.domain.models.tasks.CKTask
+import edu.stanford.cardinalkit.domain.repositories.TasksRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeTasksRepository : TasksRepository {
+
+    private val tasks = mutableListOf<CKTask>()
+
+    override fun getTasks(): Flow<Response<List<CKTask>>> {
+        return flow {
+            emit(Response.Success(tasks))
+        }
+    }
+
+    fun insertTask(task: CKTask) {
+        tasks.add(task)
+    }
+}
