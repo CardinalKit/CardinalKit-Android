aed1a4119210d9919ebf3bbbeda74ef00cc33db7
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-07-10 20:17:16 -0400

    Update TaskComponent.kt

--- app/src/main/java/edu/stanford/cardinalkit/presentation/home/components/TaskComponent.kt
+++ app/src/main/java/edu/stanford/cardinalkit/presentation/home/components/TaskComponent.kt
@@ -1,16 +1,15 @@
 package edu.stanford.cardinalkit.presentation.home.components
 
-import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import edu.stanford.cardinalkit.common.Constants
-import edu.stanford.cardinalkit.presentation.surveys.SurveyActivity
+import edu.stanford.cardinalkit.domain.models.Response
+import edu.stanford.cardinalkit.presentation.common.ProgressIndicator
 import edu.stanford.cardinalkit.presentation.tasks.TasksViewModel
-import edu.stanford.cardinalkit.presentation.home.components.SurveyTaskCard
+import edu.stanford.cardinalkit.presentation.tasks.components.TaskCard
 
 @Preview
 @Composable
@@ -18,29 +17,24 @@ fun TaskComponent(
     viewModel: TasksViewModel = hiltViewModel()
 )
 {
-    val context = LocalContext.current
-
-    // TODO: Replace hardcoded data with surveys from DB
-    val surveys = arrayOf<String>(
-        "paginated_layout_questionnaire.json",
-        "single_choice_questionnaire.json",
-        "slider_questionnaire.json"
-    )
-    LazyColumn ( userScrollEnabled = true
-    ){
-        items(surveys) { survey ->
-            SurveyTaskCard(
-                surveyName = survey,
-                context = context,
-                launchSurvey = { surveyName, context ->
-                    // Launches the SurveyActivity and passes the survey name to display
-                    val intent = Intent(context, SurveyActivity::class.java).apply {
-                        putExtra(Constants.SURVEY_NAME, surveyName)
+    when(val tasksResponse = viewModel.tasksState.value) {
+        is Response.Error -> Log.d("Tasks", tasksResponse.e?.message.toString())
+        is Response.Loading -> ProgressIndicator()
+        is Response.Success ->
+            LazyColumn(
+            ) {
+                if (tasksResponse.data != null) {
+                    items(
+                        items = tasksResponse.data
+                    ) { task ->
+                        TaskCard(
+                            title = task.title,
+                            description = task.description,
+                            category = task.context.category,
+                            uri = task.context.uri
+                        )
                     }
-                    context.startActivity(intent)
                 }
-            )
-        }
+            }
     }
-
 }
