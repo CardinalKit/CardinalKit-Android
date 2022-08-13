16d961ef6971afb8b081cc74ab3f0d16dbb566a1
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-08-13 16:45:16 -0400

    Set up permissions workflow for Health Connect

--- app/src/main/java/edu/stanford/cardinalkit/presentation/MainActivity.kt
+++ app/src/main/java/edu/stanford/cardinalkit/presentation/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
+import edu.stanford.cardinalkit.presentation.health.HealthViewModel
 import edu.stanford.cardinalkit.presentation.login.LoginViewModel
 import edu.stanford.cardinalkit.presentation.navigation.CKNavHost
 import edu.stanford.cardinalkit.presentation.navigation.Screens
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,19 +26,54 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen().apply {}
 
-        setContent {
+        /**
+         * Gets permissions for accessing health data from Health Connect
+         * if not already provided.
+         */
+        val healthViewModel by viewModels<HealthViewModel>()
+        val requestPermissionsActivityContract = healthViewModel.healthConnectManager.healthConnectClient
+            .permissionController
+            .createRequestPermissionActivityContract()
 
+        val requestPermissions = registerForActivityResult(requestPermissionsActivityContract){ granted ->
+            healthViewModel.updatePermissionsStatus(granted.containsAll(healthViewModel.permissions))
+        }
+
+        fun checkPermissionsAndRun() {
+            val permissions = healthViewModel.permissions
+            lifecycleScope.launch {
+                val granted = healthViewModel
+                    .healthConnectManager
+                    .healthConnectClient
+                    .permissionController
+                    .getGrantedPermissions(permissions)
+                if (!granted.containsAll(permissions)) {
+                    requestPermissions.launch(permissions)
+                } else {
+                    healthViewModel.updatePermissionsStatus(true)
+                }
+            }
+        }
+
+        setContent {
             val navController = rememberAnimatedNavController()
             val isAuthenticated = remember { mutableStateOf(false) }
 
-            val viewModel by viewModels<LoginViewModel>()
+            /**
+             * Observe authentication status - redirect to register/login screen
+             * if not authenticated, or main screen if authenticated.
+             */
+            val loginViewModel by viewModels<LoginViewModel>()
 
-            // start auth listener
-            viewModel.getAuthStatus().observe(this) {
+            loginViewModel.getAuthStatus().observe(this) {
                 isAuthenticated.value = it
             }
 
             if(isAuthenticated.value) {
+                // Get health data permissions if needed
+                checkPermissionsAndRun()
+
+                // Navigate to main screen
                 CKNavHost(
                     navController = navController,
                     startDestination = Screens.MainScreen.route
@@ -47,7 +84,6 @@ class MainActivity : AppCompatActivity() {
                     startDestination = Screens.JoinStudyScreen.route
                 )
             }
-
         }
     }
 }
--- app/src/main/java/edu/stanford/cardinalkit/services/HealthConnectManager.kt
+++ app/src/main/java/edu/stanford/cardinalkit/services/HealthConnectManager.kt
@@ -3,16 +3,85 @@ package edu.stanford.cardinalkit.services
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.aggregate.AggregationResult
+import androidx.health.connect.client.aggregate.AggregationResultGroupedByPeriod
+import androidx.health.connect.client.permission.Permission
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.AggregateGroupByPeriodRequest
+import androidx.health.connect.client.request.AggregateRequest
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import java.time.Instant
 import javax.inject.Inject
 
 class HealthConnectManager @Inject constructor(
     private val context: Context
     ){
-    private val healthConnectClient by lazy { HealthConnectClient.getOrCreate(context) }
+    val healthConnectClient by lazy { HealthConnectClient.getOrCreate(context) }
 
     var isAvailable = mutableStateOf(false); private set
 
     init {
         isAvailable.value = HealthConnectClient.isAvailable(context)
     }
+
+    /**
+     * Determines if all requested permissions are granted.
+     */
+    suspend fun hasAllPermissions(permissions: Set<Permission>): Boolean {
+        return permissions == healthConnectClient.permissionController.getGrantedPermissions(permissions)
+    }
+
+    /**
+     * Gets all step count records over a specified time range.
+     */
+    suspend fun readStepsByTimeRange(
+        startTime: Instant,
+        endTime: Instant
+    ): List<StepsRecord>  {
+        val response =
+            healthConnectClient.readRecords(
+                ReadRecordsRequest(
+                    StepsRecord::class,
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                )
+            )
+        return response.records
+    }
+
+    /**
+     * Aggregates step records for a particular time range.
+     */
+    suspend fun aggregateSteps(
+        startTime: Instant,
+        endTime: Instant
+    ): Long {
+        val response =
+            healthConnectClient.aggregate(
+                AggregateRequest(
+                    metrics = setOf(StepsRecord.COUNT_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime),
+                )
+            )
+        return response[StepsRecord.COUNT_TOTAL] ?: 0
+    }
+
+    /**
+     * Computes the minimum and maximum heart rate over a time range.
+     */
+    suspend fun aggregateHeartRate(
+        startTime: Instant,
+        endTime: Instant
+    ): AggregationResult {
+        val response =
+            healthConnectClient.aggregate(
+                AggregateRequest(
+                    setOf(HeartRateRecord.BPM_MAX, HeartRateRecord.BPM_MIN),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                )
+            )
+        return response
+    }
+
 }
