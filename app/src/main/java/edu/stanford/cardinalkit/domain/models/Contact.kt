46bbab6d6633c75fac4992beb2a83a7517ddc004
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-08-13 09:36:34 -0400

    Update contact model & launch map of contact location (#62)
    * Update contact model & launch map of contact location
    
    * Add all strings from contact card to localization file
    
    * Email button opens up email client with address prefilled

--- app/src/main/java/edu/stanford/cardinalkit/domain/models/Contact.kt
+++ app/src/main/java/edu/stanford/cardinalkit/domain/models/Contact.kt
@@ -2,12 +2,17 @@ package edu.stanford.cardinalkit.domain.models
 
 data class Contact(
     var id: String,
-    var name: String,
     var title: String,
+    var firstName: String,
+    var lastName: String,
+    var role: String,
     var phoneNumber: String,
-    var textNumber: String,
+    var smsNumber: String,
     var description: String,
     var email: String,
-    var addressLineOne: String,
-    var addressLineTwo: String
+    var streetAddress: String,
+    var city: String,
+    var state: String,
+    var country: String,
+    var postalCode: String
 )
